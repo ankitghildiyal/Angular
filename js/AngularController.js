1f020f97caa0f49f0e4af87880ed0f6f694e2e88
Ankit Ghildiyal <ankitghildiyal@gmail.com>
2017-03-29 19:20:14 -0500

    Fixed link to angular questions

--- js/AngularController.js
+++ js/AngularController.js
@@ -1,7 +1,7 @@
 var angularModule = angular.module('Angular', []);
 
 angularModule.controller('AngularController', ['$scope', '$http', function ($scope, $http) {
-    $http.get('js/angularQuestion.json').success(function(data){
+    $http.get('../js/angularQuestions.json').success(function(data){
         $scope.qna = data;
     });
 }]);
